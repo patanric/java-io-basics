00af5fd6057a1f40e0f0217ea1e69186889ca390
patanric <patane_riccardo@gmx.net>
2020-08-27 23:46:12 +0200

    Add simple server and client socket to show InputStream and OutputStream usage
    No treads or proper server socket closing is implemented.

---
+++ src/ch/riccardo/patane/eSocketCommunication/Client.java
@@ -0,0 +1,37 @@
+package ch.riccardo.patane.eSocketCommunication;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+
+        SocketAddress endpoint = new InetSocketAddress("localhost", 8005);
+        Socket socket = new Socket();
+        socket.connect(endpoint);
+
+        OutputStream os = socket.getOutputStream();
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(os));
+        pw.println("getMoons");
+        pw.flush();
+
+        InputStream is = socket.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        LineNumberReader lnr = new LineNumberReader(isr);
+        int number = Integer.parseInt(lnr.readLine());
+        for (int i = 0; i < number; i++) {
+            System.out.println(lnr.readLine());
+        }
+
+        socket.close();
+    }
+}
---
+++ src/ch/riccardo/patane/eSocketCommunication/Server.java
@@ -0,0 +1,41 @@
+package ch.riccardo.patane.eSocketCommunication;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(8005);
+        while (true) {
+            System.out.println("Waiting for requests.");
+
+            Socket socket = serverSocket.accept();
+            System.out.println("Request accepted.");
+
+            InputStream is = socket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            LineNumberReader lnr = new LineNumberReader(isr);
+            String request = lnr.readLine();
+            System.out.println("Request: " + request);
+
+            if (request.equals("getMoons")) {
+                OutputStream os = socket.getOutputStream();
+                PrintWriter pw = new PrintWriter(new OutputStreamWriter(os));
+                pw.println("3");
+                pw.println("Moon");
+                pw.println("Io");
+                pw.println("Europa");
+                pw.flush();
+            }
+        }
+    }
+}
